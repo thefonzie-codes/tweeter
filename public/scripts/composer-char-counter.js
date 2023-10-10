0f9a35dc17a13f3bdd6c450b332f03090fbca2d9
Alfonso <alfonsobanzon@gmail.com>
2023-10-10 19:00:01 -0400

    first commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+  const textArea = $("#tweet-text");
+
+  textArea.on('focus', function (event) {
+    $(this).text("");
+  });
+
+  textArea.on('blur', function (event) {
+    
+  });
+
+  textArea.on('input', function (event) {
+    const textLength = $(this).val().length;
+    const charCounter = $(this).parent().find('.counter');
+
+    const count = (140 - textLength);
+    charCounter.text(count);
+
+    count < 0 ? charCounter.css("color", "red") : charCounter.css("color", "#545149");
+  });
+
+});
