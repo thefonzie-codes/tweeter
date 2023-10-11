72e52cb1049b516e001dd0457b2ddb4066cdcbeb
Alfonso <alfonsobanzon@gmail.com>
2023-10-11 17:41:34 -0400

    installed timeago.js and applied it to file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = (newTweetData) => { //Creates the tweet DOM
- 
+
+  const timeSince = timeago.format(newTweetData.created_at)
+
   const layout = `
   <article>
   <section class="tweet-header">
@@ -42,7 +18,7 @@ const createTweetElement = (newTweetData) => { //Creates the tweet DOM
   </section>
   <p>${newTweetData.content.text}</p>
   <footer>
-    ${newTweetData.created_at}
+    ${timeSince}
     <section class="tweet-buttons">
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
@@ -52,7 +28,7 @@ const createTweetElement = (newTweetData) => { //Creates the tweet DOM
 </article>
 `;
 
-  $('.container').append(layout)
+  $('.container').append(layout);
 };
 
 const renderTweets = (allTweets) => { //Adds tweet DOM to container
@@ -65,15 +41,18 @@ $(document).ready(function () {
 
   const loadTweets = () => {
     $.get("http://localhost:8080/tweets", (data) => {
-      renderTweets(data)
-    })
-  }
+      renderTweets(data);
+    });
+  };
 
-  $("#new-tweet-form").on('submit',(evt) => {
+  $("#new-tweet-form").on('submit', (evt) => {
     evt.preventDefault();
-    const data = $("#new-tweet-form").serialize()
+    const data = $("#new-tweet-form").serialize();
 
     $.post("/tweets", data);
+
+    $('.container').empty();
+
     loadTweets();
   });
 
