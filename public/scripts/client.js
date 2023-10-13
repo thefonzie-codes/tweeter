1b08151505adeebb41abfd370608ccbf4cd6b691
Alfonso <alfonsobanzon@gmail.com>
2023-10-12 21:09:12 -0400

    added button at bottom right to scroll back to the top of page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,51 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (newTweetData) => { //Creates the tweet DOM
+$(document).ready(function () {
 
-  const timeSince = timeago.format(newTweetData.created_at)
+  const createTweetElement = (newTweetData) => { //Creates the tweet DOM
 
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
+    const timeSince = timeago.format(newTweetData.created_at)
+  
+    //ensures that site is safe from XSS attacks
+    //@param str is an input string which will be the new tweet data's content
+  
+    const escape = function (str) {  
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+  
+    const safeHTML = `<p>${escape(newTweetData.content.text)}</p>`;
+  
+    const layout = `
+    <article>
+    <section class="tweet-header">
+      <h4>
+      <img src="${newTweetData.user.avatars}">
+      ${newTweetData.user.name}</h4>
+      <h5>${newTweetData.user.handle}</h5>
+    </section>
+    ${safeHTML}
+    <footer>
+      ${timeSince}
+      <section class="tweet-buttons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </section>
+    </footer>
+  </article>
+  `;
+  
+    $('.existing-tweets').append(layout);
+  };
+  
+  // adds tweet DOM to container
+  // @param (allTweets) should be an array of tweet data
+  
+  const renderTweets = (allTweets) => { 
+  
+    const tweetsReverseChronological = allTweets.reverse();
+  
+    tweetsReverseChronological.forEach((element) => {
+      createTweetElement(element);
+    });
   };
 
-  const safeHTML = `<p>${escape(newTweetData.content.text)}</p>`;
-
-  const layout = `
-  <article>
-  <section class="tweet-header">
-    <h4>
-    <img src="${newTweetData.user.avatars}">
-    ${newTweetData.user.name}</h4>
-    <h5>${newTweetData.user.handle}</h5>
-  </section>
-  ${safeHTML}
-  <footer>
-    ${timeSince}
-    <section class="tweet-buttons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </section>
-  </footer>
-</article>
-`;
-
-  $('.existing-tweets').append(layout);
-};
-
-const renderTweets = (allTweets) => { //Adds tweet DOM to container
-
-  const tweetsReverseChronological = allTweets.reverse();
-
-  tweetsReverseChronological.forEach((element) => {
-    createTweetElement(element);
-  });
-};
-
-$(document).ready(function () {
+  // function  to load tweets
 
   const loadTweets = () => {
     $.get("/tweets", (data) => {
@@ -62,6 +70,8 @@ $(document).ready(function () {
 
     const input = $("#tweet-str").val();
     const data = $("#new-tweet-form").serialize();
+
+    // the code below will automatically display errors in red if tweet is 0 charachters or over 140.
 
     if (input.length > 140){
       $(".too-long").slideDown();
@@ -79,10 +89,14 @@ $(document).ready(function () {
     loadTweets();
   });
 
+  // the code below hides the error message upon clicking the tweet composer text box
+
   $("#new-tweet-form").on('click', (evt) => {
     $("h3").slideUp();
     evt.stopPropagation();
   });
+
+  // loads existing tweets upon site load
 
   loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
-  const textArea = $("#tweet-str");
+  //counts the characters within the tweet composer textbox, counter will turn red if string exceeds 140 characters.
 
-  textArea.on('input', function (event) {
+  const textArea = $("#tweet-str");  
+
+  textArea.on('input', function (event) {  
     const textLength = $(this).val().length;
     const charCounter = $(this).parent().find('.counter');
 
@@ -11,5 +13,11 @@ $(document).ready(function () {
 
     count < 0 ? charCounter.css("color", "red") : charCounter.css("color", "#545149");
   });
+
+  //scrolls window back to top if clicked
+
+  $('.scroll-up').on('click', (evt) => {
+    window.scrollTo(0, 0);
+  })
 
 });
